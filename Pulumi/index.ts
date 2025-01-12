9fceb2d3d476b05617052c0c404bcd5d93aee39c
Olukayode Arinde <108200520+olukayodedevs@users.noreply.github.com>
2025-01-12 03:02:27 -0500

    Update index.ts

--- Pulumi/index.ts
+++ Pulumi/index.ts
@@ -4,6 +4,7 @@ import * as aws from "@pulumi/aws";
 const projectName = "airtek-demo";
 const environment = "production";
 
+
 // configuration for the VPC
 const vpc = new aws.ec2.Vpc(`${projectName}-vpc`, {
 cidrBlock: "10.0.0.0/16",
